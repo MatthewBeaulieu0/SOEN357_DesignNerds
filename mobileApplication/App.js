dc67bd01327b51717dc61702bcd6a93b666f62ae
Bogdanpoda <68243088+Bogdanpoda@users.noreply.github.com>
2023-04-05 16:02:51 -0400

    firebase config re-added

--- mobileApplication/App.js
+++ mobileApplication/App.js
@@ -1,7 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {app, auth} from './firebaseConfig';
+import React,{useEffect} from 'react';
 
 export default function App() {
+  useEffect(() => {
+    console.log(app);
+
+    // Log connection status
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      console.log("hello "+user)
+      if (user) {
+        console.log('Firebase connection established.');
+      } else {
+        console.log('Firebase connection lost.');
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
