b8d74a060f2b1f74cf6ea72e89719f06e14c4da0
Bogdan Podariu <podariubogdan1@gmail.com>
2023-04-06 19:43:18 -0400

    added the second login component

---
+++ mobileApplication/Components/FormLoginComponent.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+
+const FormLoginComponent = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [age, setAge] = useState('');
+  const [country, setCountry] = useState('');
+
+  const handleFirstNameChange = (text) => {
+    setFirstName(text);
+  };
+
+  const handleLastNameChange = (text) => {
+    setLastName(text);
+  };
+
+  const handleAgeChange = (text) => {
+    setAge(text);
+  };
+
+  const handleCountryChange = (text) => {
+    setCountry(text);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <TextInput
+          style={styles.input}
+          placeholder="First Name"
+          value={firstName}
+          onChangeText={handleFirstNameChange}
+        />
+      </View>
+      <View style={styles.row}>
+        <TextInput
+          style={styles.input}
+          placeholder="Last Name"
+          value={lastName}
+          onChangeText={handleLastNameChange}
+        />
+      </View>
+      <View style={styles.row}>
+        <View style={styles.ageContainer}>
+          <TextInput
+            style={[styles.input, styles.ageInput]}
+            placeholder="Age"
+            value={age}
+            onChangeText={handleAgeChange}
+          />
+        </View>
+        <TextInput
+          style={styles.input}
+          placeholder="Country"
+          value={country}
+          onChangeText={handleCountryChange}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F2F2F2',
+    padding: 20,
+    borderRadius: 10,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#E5E5E5',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+  },
+  ageContainer: {
+    marginRight: 10,
+  },
+  ageInput: {
+    width: 60,
+  },
+});
+
+export default FormLoginComponent;
--- mobileApplication/Pages/FirstLoginPage.jsx
+++ mobileApplication/Pages/FirstLoginPage.jsx
@@ -1,104 +1,97 @@
-import React from 'react';
-import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
-import image from '../static/images/LoginLogo.png';
-import EasyLoginComponent from '../Components/EasyLoginComponent';
+import React from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import image from "../static/images/LoginLogo.png";
+import EasyLoginComponent from "../Components/EasyLoginComponent";
 
-
-
-
-
-function FirstLoginPage(){
-    const handleTurquoiseButtonClick = () => {
-        console.log('button clicked');
-        // handle turquoise button click logic here
-      }; 
-      return (
-        <View style={styles.container}>
-          <View style={styles.logoContainer}>
-            <Image
-              source={image}
-              style={styles.logo}
-              
-            />
-          </View>
-          <View style={styles.buttonContainer}>
-          <EasyLoginComponent />
-          </View>
-          
-          <TouchableOpacity style={styles.turquoiseButton} onPress={handleTurquoiseButtonClick}>
-            <Text style={styles.turquoiseButtonText}>Just a few steps away</Text>
-          </TouchableOpacity>
-          <View style={styles.pageDots}>
-        <View style={[styles.dot]} />
-        <View style={[styles.dot,styles.activeDot]} />
-        <View style={styles.dot} />
+function FirstLoginPage() {
+  const handleTurquoiseButtonClick = () => {
+    console.log("button clicked");
+    // handle turquoise button click logic here
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image source={image} style={styles.logo} />
       </View>
-        </View>
-      );
+      <View style={styles.buttonContainer}>
+        <EasyLoginComponent />
+      </View>
+      <TouchableOpacity
+        style={styles.turquoiseButton}
+        onPress={handleTurquoiseButtonClick}>
+        <Text style={styles.turquoiseButtonText}>Just a few steps away</Text>
+      </TouchableOpacity>
+      <View style={styles.pageDots}>
+        <View style={[styles.dot]}/>
+        <View style={[styles.dot, styles.activeDot]} />
+        <View style={styles.dot}/>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#f2f2f2', 
-    },
-    logoContainer: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      height:'28%',
-      marginTop:-70,
-      marginBottom: 10
-    },
-    logo: {
-      width:'50%',
-      height:'100%',
-      resizeMode:"contain",
-      borderRadius: 10,
-      aspectRatio: 1, // set the aspect ratio of your logo
-    },
-    turquoiseButton: {
-      marginTop:30,
-      backgroundColor: 'turquoise',
-      paddingVertical: 16,
-      paddingHorizontal: 32,
-      borderRadius: 50,
-      alignItems: 'center',
-      justifyContent: 'center',
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.2,
-      shadowRadius: 10,
-      elevation: 3,
-    },
-    turquoiseButtonText: {
-      color: '#fff',
-      fontWeight: 'bold',
-      fontSize: 18,
-    },
-    buttonContainer:{
-      marginTop:10,
-      height:'38%',
-      width:'80%',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    pageDots: {
-      flexDirection: 'row',
-     
-      marginTop: 20,
-    },
-  
-    dot: {
-      width: 10,
-      height: 10,
-      borderRadius: 5,
-      backgroundColor: '#c4c4c4',
-      marginHorizontal: 5,
-    },
-    activeDot: {
-      backgroundColor: 'turquoise',
-    },
-  });
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f2f2f2",
+  },
+  logoContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: "28%",
+    marginTop: -70,
+    marginBottom: 10,
+  },
+  logo: {
+    width: "50%",
+    height: "100%",
+    resizeMode: "contain",
+    borderRadius: 10,
+    aspectRatio: 1, // set the aspect ratio of your logo
+  },
+  turquoiseButton: {
+    marginTop: 30,
+    backgroundColor: "turquoise",
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+    elevation: 3,
+  },
+  turquoiseButtonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  buttonContainer: {
+    marginTop: 10,
+    height: "38%",
+    width: "80%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  pageDots: {
+    flexDirection: "row",
+
+    marginTop: 20,
+  },
+
+  dot: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: "#c4c4c4",
+    marginHorizontal: 5,
+  },
+  activeDot: {
+    backgroundColor: "turquoise",
+  },
+});
 export default FirstLoginPage;
